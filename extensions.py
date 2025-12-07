bda757e3ef0ebe3536704a9c31862268367fa0d3
Suthakar Thiroshan <sthiroshan@gmail.com>
2025-12-07 10:49:21 +0530

    Fixed some bugs in extension program

--- extensions.py
+++ extensions.py
@@ -7,14 +7,14 @@
 # .jpg => image/jpeg 
 # .jpeg => image/jpeg 
 # .txt => text/plain 
-# .zip => text/plain 
+# .zip => application/zip 
 # .pdf => application/pdf 
 # Other suffixes or no suffix at all output "application/octet-stream" 
 #----------------------------
 
 
 file_name = input("File name: ").strip().casefold()  # Makes everything small case and no side spaces
-_ , extension = file_name.split(".") # Change the file name into list Ex : cat.jpg => ['cat','jpg']
+extension = file_name.split(".")[-1] # Change the file name into list Ex : cat.jpg => ['cat','jpg']
 
 match extension:
 
@@ -27,11 +27,14 @@ match extension:
     case "jpg" | "jpeg":
         mime = "image/jpeg" 
 
-    case "txt" | "zip":
+    case "txt":
         mime = "text/plain" 
 
     case "pdf":
         mime = "application/pdf" 
+    
+    case "zip":
+        mime = "application/zip" 
 
     case _:
         mime = "application/octet-stream"
